2e5e043e3232843f5a56087a10f11d2e8ea6d2e4
NOME <git@gmail>
2023-02-28 17:37:08 -0300

    Funcionario: Controller e View |* Inserir os dados do funcionario *

--- src/main/java/com/jmRocha/web/controller/FuncionarioController.java
+++ src/main/java/com/jmRocha/web/controller/FuncionarioController.java
@@ -1,19 +1,56 @@
 package com.jmRocha.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.jmRocha.domain.Cargo;
+import com.jmRocha.domain.Funcionario;
+import com.jmRocha.domain.UF;
+import com.jmRocha.service.CargoService;
+import com.jmRocha.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	@Autowired
+	private CargoService cargoService;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarTodos());
 		return "/funcionario/lista"; 
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
 	}
 }
