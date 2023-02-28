1582b387de7d1b9a278635e0805af95fc69de4aa
NOME <git@gmail>
2023-02-28 17:32:54 -0300

    Departamento: Controller e View |* Inserir, Listar, Editar, Excluir, alerta de sucesso *

--- src/main/java/com/jmRocha/dao/CargoDaoImpl.java
+++ src/main/java/com/jmRocha/dao/CargoDaoImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import com.jmRocha.domain.Cargo;
 
+
 @Repository
 public class CargoDaoImpl extends AbstractDao<Cargo, Long> implements CargoDao {
 
