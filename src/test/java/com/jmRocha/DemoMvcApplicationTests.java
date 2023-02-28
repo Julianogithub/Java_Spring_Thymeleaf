7ac72378a49cc5ffdd3b1ee7d46fb2fcc1c81307
NOME <git@gmail>
2023-02-28 16:43:56 -0300

    Criando o Pregeto | Configurando a pagina Home no Controller

---
+++ src/test/java/com/jmRocha/DemoMvcApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jmRocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+ 
+}
